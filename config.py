88780c3470c497dd6215ab1645bf8df4faffcfee
Eloi Le Renard <eloi.fun@hotmail.fr>
2022-11-15 23:15:18 +0100

    unit test bot deployed

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "c66c0f71-d746-4b8e-9d04-586c7fe1b7d9")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "07b52f07b3b248b992727f9910b03a77")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "6efdf1c2-725f-41ff-9bc5-68d0dbbfba4f")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "2a409845f8024e2d974667747602760d")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "res-namep10.cognitiveservices.azure.com")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "d97277b9-e6dc-46a1-b8db-03c0d49332c5"
+        "AppInsightsInstrumentationKey", "d4432566-3d2f-4c41-b458-c45c646a33c3"
     )
