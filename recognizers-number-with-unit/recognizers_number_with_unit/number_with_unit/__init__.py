95552fcb52ffa0b41dd4cf0815c54460185f2fdd
Eloi Le Renard <eloi.fun@hotmail.fr>
2022-11-17 14:22:36 +0100

    ijuhygh

---
+++ recognizers-number-with-unit/recognizers_number_with_unit/number_with_unit/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License.
+
+from .number_with_unit_recognizer import *
+from .constants import *
+from .models import *
+from .extractors import *
+from .parsers import *
+from .chinese import *
