cc6d378fe6b1a852308c0fbb3769eb999b243c78
Eloi Le Renard <eloi.fun@hotmail.fr>
2022-11-19 21:32:55 +0100

    flask v0

--- app.py
+++ app.py
@@ -89,8 +89,31 @@ async def messages(req: Request) -> Response:
 APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
 APP.router.add_post("/api/messages", messages)
 
-if __name__ == "__main__":
-    try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
-    except Exception as error:
-        raise error
+#if __name__ == "__main__":
+ #   try:
+ #       web.run_app(APP, host="localhost", port=CONFIG.PORT)
+ #   except Exception as error:
+ #       raise error
+
+
+
+from flask import Flask, request, jsonify, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/predict_api', methods=["GET","POST"])
+def list_post():
+    json_body = request.get_json()
+    predictions = 2 * json_body[0]   
+    predictions = list(predictions)
+    return jsonify(results = predictions)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
+    
+    
+    
+    
