855098873d5b7ce38526fd6cd6fc1b8d397ac7f7
Eloi Le Renard <eloi.fun@hotmail.fr>
2022-12-11 12:43:58 +0100

    Update unit_test_tuto.py

--- unit_test_tuto.py
+++ unit_test_tuto.py
@@ -39,14 +39,21 @@ class EmailPromptTest(aiounittest.AsyncTestCase):
 
         adapter = TestAdapter(exec_test)
         conv_state = ConversationState(MemoryStorage())
-
         dialogs_state = conv_state.create_property("dialog-state")
         dialogs = DialogSet(dialogs_state)
         dialogs.add(BookingDialog("dialog_id"))
-
         step1 = await adapter.test('Hello', 'To what city would you like to travel?')
 #        step1 = await adapter.test('Hello', 'What can I help you with today?')
         step2 = await step1.send('paris')
 #        step2 = await step1.send('book a flight')
         await step2.assert_reply("From what city will you be travelling?")
+
+        adapter = TestAdapter(exec_test)
+        conv_state = ConversationState(MemoryStorage())
+        dialogs_state = conv_state.create_property("dialog-state")
+        dialogs = DialogSet(dialogs_state)
+        dialogs.add(BookingDialog("dialog_id"))
+        step1 = await adapter.test('book a flight to paris from berlin for the 11/11/22 with budget of 999$', 'return date ?')
+        step2 = await step1.send('11/11/22')
+        await step2.assert_reply("Please confirm, I have you traveling to: Paris from: Berlin on: 2022-11-11. return: 11/11/22 for max: 999 $.")
 
