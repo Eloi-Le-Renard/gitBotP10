0d939b13cd8c029f0e5e75df60d28363a03e608a
Eloi Le Renard <eloi.fun@hotmail.fr>
2022-12-17 18:42:54 +0100

    final

--- brouillon.py
+++ brouillon.py
@@ -1,3 +1,17 @@
+        adapter = TestAdapter(exec_test)
+        conv_state = ConversationState(MemoryStorage())
+        dialogs_state = conv_state.create_property("dialog-state")
+        dialogs = DialogSet(dialogs_state)
+        dialogs.add(BookingDialog("dialog_id"))
+        
+        step1 = await adapter.test('book a flight to paris from berlin for the 11/11/22 with budget of 999$', 'return date ?')
+        step2 = await step1.send('11/11/22')
+        await step2.assert_reply("Please confirm, I have you traveling to: Paris from: Berlin on: 2022-11-11. return: 11/11/22 for max: 999 $.")
+
+step1 = await adapter.test('Hello', 'To what city would you like to travel?')
+        step2 = await step1.send('paris')
+        await step2.assert_reply("From what city will you be travelling?")
+
 from dialogs.booking_dialog import BookingDialog
 from booking_details import BookingDetails
 
--- unit_test_tuto.py
+++ unit_test_tuto.py
@@ -45,7 +45,7 @@ class EmailPromptTest(aiounittest.AsyncTestCase):
         dialogs.add(BookingDialog("dialog_id"))
 
 #        step1 = await adapter.send('book a flight to paris from berlin for the 11/11/22 with budget of 999$')
-        step1 = await adapter.send('peut importe')
+        step1 = await adapter.send('trigger')
         step2 = await step1.assert_reply("To what city would you like to travel?")
         step3 = await step2.send('paris')
         step4 = await step3.assert_reply("From what city will you be travelling?")
@@ -61,7 +61,7 @@ class EmailPromptTest(aiounittest.AsyncTestCase):
         dialogs = DialogSet(dialogs_state)
         dialogs.add(BookingDialog("dialog_id"))
 
-        step1 = await adapter.send('peut importe')
+        step1 = await adapter.send('trigger')
         step2 = await step1.assert_reply("To what city would you like to travel?")
         step3 = await step2.send('paris')
         step4 = await step3.assert_reply("From what city will you be travelling?")
@@ -75,6 +75,28 @@ class EmailPromptTest(aiounittest.AsyncTestCase):
         step12 = await step11.assert_reply("Please confirm, I have you traveling to: paris from: berlin on: 2022-11-11. return: 12/11/22 for max: 999$.")
         step13 = await step12.send('yes')
         step_final = await step13.assert_reply("you say YES, i confirmed your flight")
+
+        # dialog test 3
+        adapter = TestAdapter(exec_test)
+        conv_state = ConversationState(MemoryStorage())
+        dialogs_state = conv_state.create_property("dialog-state")
+        dialogs = DialogSet(dialogs_state)
+        dialogs.add(BookingDialog("dialog_id"))
+
+        step1 = await adapter.send('trigger')
+        step2 = await step1.assert_reply("To what city would you like to travel?")
+        step3 = await step2.send('paris')
+        step4 = await step3.assert_reply("From what city will you be travelling?")
+        step5 = await step4.send('berlin')
+        step6 = await step5.assert_reply("On what date would you like to travel?")
+        step7 = await step6.send('11/11/22')
+        step8 = await step7.assert_reply("return date ?")
+        step9 = await step8.send('12/11/22')
+        step10 = await step9.assert_reply("budget ?")
+        step11 = await step10.send('999$')
+        step12 = await step11.assert_reply("Please confirm, I have you traveling to: paris from: berlin on: 2022-11-11. return: 12/11/22 for max: 999$.")
+        step13 = await step12.send('bplgfpobl')
+        step_final = await step13.assert_reply("you say NO, let's restart")
 
 
 
